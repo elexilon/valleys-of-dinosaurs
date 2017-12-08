f88d6e2a92aff429838ac68ffb8c4c87b3ea69d3
ELEXILON <elexilon@gmail.com>
2017-12-08 17:28:57 +0100

    MODIFY

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,4 +1,10 @@
 class Dinosaur < ApplicationRecord
+  belongs_to :valley
+  validates :name, :age, presence: true
+  validates :name, uniqueness: true
+
+  validates :age, length: { maximum: 5 }, allow_blank: true
+  validates :image_url, length: { maximum: 255 }, allow_blank: true
 
   def self.order_by_name
       order(name: :asc)
